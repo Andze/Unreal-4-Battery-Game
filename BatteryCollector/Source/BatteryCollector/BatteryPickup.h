1c7aa0694fae212635bf5523a1cd6c3e35777dc9
Andrew Cardwell <andrewcardwell@btinternet.com>
2016-10-05 13:18:02 +0100

    Tutorial 5: Extending the Pickup Class

---
+++ BatteryCollector/Source/BatteryCollector/BatteryPickup.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Pickup.h"
+#include "BatteryPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTERYCOLLECTOR_API ABatteryPickup : public APickup
+{
+	GENERATED_BODY()
+	
+public:
+	//Set default values for the actors properties
+	ABatteryPickup();
+	
+	
+};
