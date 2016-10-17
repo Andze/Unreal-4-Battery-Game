79ecf7b77822656d3b413a6045359f0eac5e1fc6
Andrew Cardwell <andrewcardwell@btinternet.com>
2016-10-17 13:36:57 +0100

    Tutorial 16: Enabling UMG

--- BatteryCollector/Source/BatteryCollector/BatteryCollectorGameMode.h
+++ BatteryCollector/Source/BatteryCollector/BatteryCollectorGameMode.h
@@ -13,10 +13,28 @@ public:
 
 	virtual void Tick(float DeltaTime) override;
 
+	//retunrs power to win for HUD
+	UFUNCTION(BlueprintPure, Category = "power")
+	float GetPowerToWin() const;
+
+	virtual void BeginPlay() override;
+
 protected:
 	//Decay Rate of character
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power", Meta = (BlueprintProtected = "true"))
 	float DecayRate;
+
+	//power to win the game
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power", Meta = (BlueprintProtected = "true"))
+	float PowerToWin;
+
+	//The widget class to use for our HUD screen
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Power", Meta = (BlueprintProtected = "true"))
+	TSubclassOf<class UUserWidget> HUDWidgetClass;
+
+	//the instance of the HUD
+	UPROPERTY()
+	class UUserWidget* CurrentWidget;
 };
 
 
